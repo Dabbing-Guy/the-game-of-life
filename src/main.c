190c32e8d961b04d19558fcccb4bb2ce53ad9415
Michael Stocki <mistocki@student.linnmar.org>
2021-12-04 15:39:59 -0600

    Make a screen and fuction to print it

--- src/main.c
+++ src/main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 
+#define SCREEN_Y 10
+#define SCREEN_X 35
+const char ALIVE = 'X';
+const char DEAD = ' ';
+
+void printScreen(const char screen[SCREEN_Y][SCREEN_X])
+{
+    for (int y = 0; y < SCREEN_Y; y++)
+    {
+        for (int x = 0; x < SCREEN_X; x++)
+        {
+            printf("%c", screen[y][x]);
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
-    printf("main.c has built sucessfully and is now running!\n");
+    char screen[SCREEN_Y][SCREEN_X];
+
+    // Fill screen with dead cells
+    for (int y = 0; y < SCREEN_Y; y++)
+    {
+        for (int x = 0; x < SCREEN_X; x++)
+        {
+            screen[y][x] = DEAD;
+        }
+    }
+
+    // Main Loop
+    while (1)
+    {
+        printScreen(screen);
+    }
     return 0;
 }
